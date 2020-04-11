2f8236d944be460a10f6519f3e6f6bf20ef99b71
raeray <mirae@hungrycoco.com>
2020-04-11 12:21:25 -0700

    predict file

---
+++ predict.py
@@ -0,0 +1,43 @@
+import logging
+from gensim.models import LdaModel
+from gensim import corpora
+import nltk
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import stopwords 
+
+
+class Predict():
+    def __init__(self, lda_model_path, dictionary_path):
+        lda_model_path = lda_model_path
+        self.dictionary = corpora.Dictionary.load(dictionary_path)
+        self.lda = LdaModel.load(lda_model_path)
+
+    def load_stopwords(self):
+        return set(stopwords.words('english')) 
+
+    def extract_lemmatized_nouns(self, new_review):
+        stopwords = self.load_stopwords()
+        words = []
+
+        sentences = nltk.sent_tokenize(new_review.lower())
+        for sentence in sentences:
+            tokens = nltk.word_tokenize(sentence)
+            text = [word for word in tokens if word not in stopwords]
+            tagged_text = nltk.pos_tag(text)
+
+            for word, tag in tagged_text:
+                words.append({"word": word, "pos": tag})
+
+        lem = WordNetLemmatizer()
+        nouns = []
+        for word in words:
+            if word["pos"] in ["NN", "NNS"]:
+                nouns.append(lem.lemmatize(word["word"]))
+
+        return nouns
+
+    def run(self, new_review):
+        nouns = self.extract_lemmatized_nouns(new_review)
+        new_review_bow = self.dictionary.doc2bow(nouns)
+        new_review_lda = self.lda[new_review_bow]
+        return new_review_lda
